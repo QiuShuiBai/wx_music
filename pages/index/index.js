271e329445e382a36059f8770ee9114101aae78c
QiuShuiBai <1125097005@qq.com>
2017-11-30 01:16:35 +0800

    完成顶部盒子初步设计

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    currentTab: 0,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,34 +12,32 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
+
+  // 界面的改变，由数据决定
+  // 界面的状态 === 数据的值
+  // mvvm区分于dom api的本质
+  clickTab: function(e){ 
+    // 状态改变，data binding 值 
+    console.log(e);
+    var index = e.target.dataset.sss;
+    this.setData({
+      currentTab:index,
+    });
   },
+
+  swiperTab: function(e){
+    console.log(e);
+    var index = e.detail.current;
+    this.setData({
+      currentTab:index,
+    });
+  },
+
+  onLoad: function () {
+
+  },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
