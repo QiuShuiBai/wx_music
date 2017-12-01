b2b0bb5831fd30ff550bf83aab0d3f74e445d7f2
QiuShuiBai <1125097005@qq.com>
2017-12-02 01:50:08 +0800

    做好了首页的处理，准备获取数据填充页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,40 @@ const app = getApp()
 
 Page({
   data: {
-    currentTab: 0,
-    autoplay:true,
-    interval:1000,
-    duration:1000,
-    circular:true,
-    indicatorDots:true
+      currentTab: 0,
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
   },
 
 
   // 界面的改变，由数据决定
   // 界面的状态 === 数据的值
   // mvvm区分于dom api的本质
-  clickTab: function(e){ 
-    // 状态改变，data binding 值 
-    console.log(e);
-    var index = e.target.dataset.sss;
-    this.setData({
-      currentTab:index,
-    });
+  clickTab: function (e) {
+      // 状态改变，data binding 值 
+      console.log(e);
+      var index = e.target.dataset.sss;
+      this.setData({
+          currentTab: index,
+      });
   },
 
-  swiperTab: function(e){
-    console.log(e);
-    var index = e.detail.current;
-    this.setData({
-      currentTab:index,
-    });
+  swiperTab: function (e) {
+      console.log(e);
+      var index = e.detail.current;
+      this.setData({
+          currentTab: index,
+      });
   },
+
 
   onLoad: function () {
 
   },
 
   getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+   
   }
 })
