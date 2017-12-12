51c93aee448ba6371bc797dc7308cf1da135defa
QiuShuiBai <1125097005@qq.com>
2017-12-12 23:15:58 +0800

    添加了'我的歌单'页面跳转功能

--- pages/music/music.js
+++ pages/music/music.js
@@ -1,7 +1,27 @@
+const app = getApp()
 Page({
-    data:{
-
+    data: {
+        wRun: 0,
+        musicList: [{
+                'url': '../../images/music.png',
+                'words': '本地音乐',
+                'num': '53'
+            },{'url': '../../images/history.png',
+                'words': '最近播放',
+                'num': '100'
+            },{
+                'url': '../../images/radio.png',
+                'words': '我的电台', 
+                'num': '0'
+            },{ 
+                'url': '../../images/collection.png',
+                'words': '我的收藏', 
+                'num': '6'
+            }]
     },
-    onLoad: function () {
-    }
+   onShow: function () {
+        this.setData({
+            wRun: app.globalData.running
+        })
+    },
 })
