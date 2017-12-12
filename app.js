34cf0a437978fc052c62c5e2392bdc260e77fb4d
QiuShuiBai <1125097005@qq.com>
2017-12-12 22:17:35 +0800

    实现全局加载数据

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+      wx.request({
+          url:"https://www.easy-mock.com/mock/5a2ac29a0d73d175e0478566/music/info",
+          success: (response) => {
+              this.globalData.music =  response.data;
+              this.globalData.nowMusic = response.data.music;
+          }
+      })
   },
   globalData: {
     running: 0,
     music:'',
-    i:0
+    i:0,
+    data:0,
+    num:-1,
+    nowMusic:''
   }
 })
