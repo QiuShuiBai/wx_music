2159ee021b1d24092b25a247318d550c573ce252
QiuShuiBai <1125097005@qq.com>
2017-12-12 22:31:53 +0800

    完成账号静态页面

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,7 +1,37 @@
 Page({
-    data:{
-
+    data: {
+        musicList: [{
+            'url': '../../images/music.png',
+            'words': '我的消息',
+        }, {
+            'url': '../../images/history.png',
+            'words': '会员中心',
+        }, {
+            'url': '../../images/radio.png',
+            'words': '商城',
+        }, {
+            'url': '../../images/collection.png',
+            'words': '在线听歌免流量',
+        },
+        {
+            'url': '../../images/music.png',
+            'words': '设置',
+        }, {
+            'url': '../../images/history.png',
+            'words': '扫一扫',
+        }, {
+            'url': '../../images/radio.png',
+            'words': '个性换肤',
+        }, {
+            'url': '../../images/collection.png',
+            'words': '在线听歌免流量',
+        }, {
+            'url': '../../images/radio.png',
+            'words': '定时关闭',
+        }, {
+            'url': '../../images/collection.png',
+            'words': '关于',
+        }]
     },
-    onLoad: function () {
-    }
+
 })
